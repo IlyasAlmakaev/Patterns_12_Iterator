70dc6d0852c46b4abbf8d2472a192d6cba487511
Ильяс <i.almakaev@gmail.com>
2017-08-04 14:51:14 +0700

    Initial Commit

---
+++ Patterns_12_Iterator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Patterns_12_Iterator
+//
+//  Created by Ильяс on 04.08.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Patterns_12_Iterator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Patterns_12_Iterator
+//
+//  Created by Ильяс on 04.08.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
